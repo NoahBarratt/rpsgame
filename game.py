2cee6081a6416abbe8ee4c3aed013c8e2d5de61d
NoahBarratt <89535538+NoahBarratt@users.noreply.github.com>
2021-09-21 11:59:24 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,7 +1,9 @@
 # this is the "game.py" file...
 
 import random
+from typing_extensions import ParamSpecArgs
 
+print("Welcome 'Player One' to my Rock-Paper-Scissors game...")
 print("Rock, Paper, Scissors, Shoot!")
 
 # PROMPT USER FOR INPUT
@@ -16,4 +18,43 @@ print(user_choice)
 options = ["rock", "paper", "scissors"]
 
 computer_choice = random.choice(options)
+print("Computer chose:")
 print(computer_choice)
+
+if computer_choice == "rock" and user_choice == "scissors":
+    print("Oh, the computer won. It's ok.")
+    print("Thanks for playing. Please play again!")
+
+if computer_choice == "scissors" and user_choice == "paper":
+    print("Oh, the computer won. It's ok.")
+    print("Thanks for playing. Please play again!")
+
+if computer_choice == "rock" and user_choice == "paper":
+    print("Congragulations, you won!")
+    print("Thanks for playing. Please play again!")
+
+if computer_choice == "paper" and user_choice == "scissors":
+    print("Congragulations, you won!")
+    print("Thanks for playing. Please play again!")
+
+if computer_choice == "scissors" and user_choice == "rock":
+    print("Congragulations, you won!")
+    print("Thanks for playing. Please play again!")
+
+if computer_choice == "paper" and user_choice == "rock":
+    print("Oh, the computer won. It's ok.")
+    print("Thanks for playing. Please play again!")
+
+if computer_choice == "paper" and user_choice == "paper":
+    print("It's a tie!")
+    print("Thanks for playing. Please play again!")
+
+if computer_choice == "rock" and user_choice == "rock":
+    print("It's a tie!")
+    print("Thanks for playing. Please play again!")
+
+if computer_choice == "scissors" and user_choice == "scissors":
+    print("It's a tie!")
+    print("Thanks for playing. Please play again!")
+breakpoint()
+
